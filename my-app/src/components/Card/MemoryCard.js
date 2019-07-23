3a4899a94c3ee7f0f3f91bc4d61405b30df866a1
Robert Rowan <rowanb27@yahoo.com>
2019-07-23 17:06:08 -0500

    completed MemoryCard component

--- my-app/src/components/Card/MemoryCard.js
+++ my-app/src/components/Card/MemoryCard.js
@@ -5,6 +5,11 @@ const MemoryCard = props => (
     <div className="allCards col-xs-6 col-sm-6 col-md-4"
     key={props.id}
     onClick= {() => props.handleClick(props.id, props.clicked)}>
-        <img className=""
+        <img className="img-fluid width: 100% \9"
+        id={props.name}
+        src={props.image}
+        alt={props.name}></img>
     </div>
-)
+);
+
+export default MemoryCard;
